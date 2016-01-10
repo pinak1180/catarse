43c48729e66fe4cdf357c2853bd1d19f1c018f07
pinak <pinak.0605@gmail.com>
2016-01-10 15:22:27 +0530

    improved session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.production? && CatarseSettings.get_without_cache(:base_domain)
-  Catarse::Application.config.session_store :cookie_store, key: CatarseSettings.get_without_cache(:secret_token), domain: CatarseSettings.get_without_cache(:base_domain)
+  Catarse::Application.config.session_store :cookie_store, key: CatarseSettings.get_without_cache(:secret_token)#, domain: CatarseSettings.get_without_cache(:base_domain)
 else
   Catarse::Application.config.session_store :cookie_store, key: CatarseSettings.get_without_cache(:secret_token)
 end
@@ -10,4 +10,3 @@ end
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
 # Catarse::Application.config.session_store :active_record_store
-
